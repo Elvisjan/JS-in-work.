b5dfb15d01d8b9f70431f5a326ea2c70c21a8710
Elvisjan <46963377+Elvisjan@users.noreply.github.com>
2019-03-19 21:43:22 +0500

    Таймер

--- йога/script.js
+++ йога/script.js
@@ -30,4 +30,57 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     }
   });
+  // Timer
+  let deadline = '2019-03-20';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / 1000 / 60 / 60) % 24 - 4),
+      days = Math.floor((t/(1000 * 60 * 60 * 24)));
+  
+    return  {
+      'total': t,
+      'seconds': seconds,
+      'minutes': minutes,
+      'hours': hours,
+      'days' : days
+    };
+  };
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds');
+      days = timer.querySelector('.days')
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {      
+      let t = getTimeRemaining(endtime);
+      hours.textContent = t.hours;      
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;      
+      days.textContent = t.days;
+      if(seconds.textContent.length < 2) {
+        seconds.textContent = '0' + t.seconds
+      };
+      if(minutes.textContent.length < 2) {
+        minutes.textContent = '0' + t.minutes
+      };
+      if(hours.textContent.length < 2) {
+        hours.textContent = '0' + t.hours
+      };
+      if(days.textContent.length < 2) {
+        days.textContent = '0' + t.days
+      };
+      if (t.total <=0) {
+        clearInterval(timeInterval);
+      }
+    }
+
+  
+}
+setClock('timer', deadline);
 });
