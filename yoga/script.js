003944204eb867be77a1bd448a66c37257ee6eff
Elvisjan <46963377+Elvisjan@users.noreply.github.com>
2019-03-22 12:52:21 +0500

    Формы и сервер

--- yoga/script.js
+++ yoga/script.js
@@ -105,4 +105,69 @@ window.addEventListener('DOMContentLoaded', function () {
       document.body.style.overflow = 'hidden'
     });
   };
+  //Form
+  let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+  };
+  let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+  statusMessage.classList.add('status');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+    let request = new XMLHttpRequest();
+    request.open('POST', 'http://127.0.0.1:8080/yoga/server.js');
+    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    let formData = new FormData(form);
+    let obj = {};
+    for (const [key, value] of formData.entries()) {
+      obj[key] = value;
+    };
+    let jsonObj = JSON.stringify(obj);
+    request.send(jsonObj);
+    request.addEventListener('readystatechange', function () {
+      if (request.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+      } else if (request.readyState === 4 && request.status == 200) {
+        statusMessage.innerHTML = message.success;
+      } else {
+        statusMessage.innerHTML = message.failure;
+      }
+    });
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = '';
+    }
+  });
+  //Contacts
+  let contacts = document.getElementById('form'),
+  contactsInput = contacts.getElementsByTagName('input');
+  contacts.addEventListener('submit', function(event){
+    event.preventDefault();
+    contacts.appendChild(statusMessage);
+    let request1 = new XMLHttpRequest();
+    request1.open('POST', 'http://127.0.0.1:8080/yoga/server.js');
+    request1.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    let formData1 = new FormData(contacts);
+    let obj1 = {};
+    for (const [key, value] of formData1.entries()) {
+      obj1[key] = value;
+    };
+    let jsonObj1 = JSON.stringify(obj1);
+    request1.send(jsonObj1);
+    request1.addEventListener('readystatechange', function () {
+      if (request1.readyState < 4) {
+        statusMessage.textContent = message.loading;
+      } else if (request1.readyState === 4 && request1.status == 200) {
+        statusMessage.textContent = message.success;
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+    });
+    for (let i = 0; i < contactsInput.length; i++) {
+      contactsInput[i].value = '';
+    }
+  });
 });
